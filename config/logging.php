eedb4b867d2ca1c591578017bf784bf218462e5e
rietzche <haritzrahman98@gmail.com>
2019-01-28 15:51:54 +0700

    link pdf

--- config/logging.php
+++ config/logging.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'single'),
+    'default' => env('LOG_CHANNEL', 'stack'),
 
     /*
     |--------------------------------------------------------------------------
